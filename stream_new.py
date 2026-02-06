9ba39b50ae39a067bd0659823341397d5c702499
trivenirln-cyber <triveni.rln@gmail.com>
2026-02-06 13:52:30 +0530

    Add files via upload

---
+++ stream_new.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("My First Navigation App")
+st.text("Where you want to go today?")
+st.page_link("https://www.google.com/",label = "Go to Google")
+st.page_link("https://streamlit.io/",label = "Go to Streamlit")
